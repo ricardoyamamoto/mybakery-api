6c399f95c04469223c4e283d15e56d4dcf3dafe9
lischiang <lisa.chiang.sv@gmail.com>
2017-07-19 17:41:25 -0400

    Removed uppercase sensitivity for Search
    Removed uppercase sensitivity for Search Ingredients and Recipes

--- src/facades/search-by-title.js
+++ src/facades/search-by-title.js
@@ -8,19 +8,25 @@ class SearchByTitleFacade{
     find(...args) {
 
         var regExp1 = ''; // regExp2 = '';
+        // var perPage = 3;       
+        // var page = 0;
+        
+        if (args[0].page)
+        {
+            page = Math.max(0, args[0].page);
+        }        
 
         if (args[0].q) {      
             regExp1 = '.*' + args[0].q + '.*';
         }
 
         return this.Schema
-        .find({title : new RegExp(regExp1)})
-        // .sort(searchObj.sort)
+        .find({"title" : { $regex: new RegExp(regExp1, "i") }}) 
         .populate('author category recipeIngredients.ingredient recipeIngredients.unit')
+        // .limit(perPage)
+        // .skip(perPage * page)
         .sort({lastModified: -1})
         .exec();
-
-       
     }
 }
 
