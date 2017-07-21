5065f23739196fbe0c885f008f34b109c77c34b2
guo <yujieguo1990@gmail.com>
2017-07-21 09:13:29 -0400

    update recipe pagination

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -1,6 +1,18 @@
 const Controller = require('../lib/controller');
 const recipeFacade = require('../facades/recipe');
 
-class RecipeController extends Controller {}
+class RecipeController extends Controller {
+
+    constructor(schema) {
+        super(schema);
+    }
+    find(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        return this.facade.find(req.query, function(collection) {
+                    res.status(200).json(collection);
+            });
+    }
+ }
 
 module.exports = new RecipeController(recipeFacade);
