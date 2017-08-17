b7969d5a2bb4ecbde4a87e657c1236d0d6aef7fc
lischiang <lisa.chiang.sv@gmail.com>
2017-08-16 21:19:46 -0400

    Added quantity field to ingredient model

--- src/models/ingredient.js
+++ src/models/ingredient.js
@@ -6,6 +6,7 @@ const ingredientSchema = new Schema({
     name: { type: String, required: true, index: true },
     description: { type: String, required: true },
     defaultUnit: { type: Schema.Types.ObjectId, ref: 'Unit'},
+    quantity: { type: Number, required: false},
     cost: { type: Number, required: false}
 });
 
